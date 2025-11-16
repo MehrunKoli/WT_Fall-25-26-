20924a7589ec59c43f771c4fe3fb485217a59f4f
MehrunKoli <mehrunkoli03@gmail.com>
2025-11-16 12:30:39 +0600

    dn

---
+++ Mid/Lab_task02.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded',() => {
+
+const signupform = document.getElementById('signup-form');
+const fullnameInput = document.getElementById('fullname');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const confirmpasswordInput = document.getElementById('confirmpassword');
+const signupInput = document.getElementById('signup-btn');
+
+const courseInput = document.getElementById('coursename');
+const addCourseBtn = document.getElementById('addCourseBtn');
+const courseList = document.getElementById('courseList');
+const noCourse = document.getElementById('no-courses');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+)

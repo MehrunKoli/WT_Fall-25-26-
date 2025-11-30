21b4c8e7ea6a456bbd79ebb246b72f31f99ff7f1
MehrunKoli <mehrunkoli03@gmail.com>
2025-12-01 04:57:12 +0600

    dn

--- Mid/Lab_task02.js
+++ Mid/Lab_task02.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener("click",function() {
 
 const signupform = document.getElementById('signup-form');
 const fullnameInput = document.getElementById('fullname');
@@ -11,7 +11,22 @@ const courseInput = document.getElementById('coursename');
 const addCourseBtn = document.getElementById('addCourseBtn');
 const courseList = document.getElementById('courseList');
 const noCourse = document.getElementById('no-courses');
+const heading = document.querySelector("#heading-last");
 
+
+if(fullnameInput.value==="" || emailInput.value===""|| passwordInput.value==="" || confirmpasswordInput.value===""){
+
+    heading.innerHTML="Fillup all fields";
+}
+if(passwordInput.value.length<8){
+    heading.innerHTML="Password must contain 8 character";
+}
+
+else{
+    heading.innerHTML="Reg successful";
+}
+
+const btnReg = document.querySelector("#signup-btn");
 
 
 
